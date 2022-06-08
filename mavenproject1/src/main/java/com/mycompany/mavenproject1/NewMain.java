0088d01bab9104da9cd047583b4c9ce9c40ee7af
1988029 <1988029@alu.murciaeduca.es>
2022-06-08 09:00:39 +0200

    texto gracioso

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewMain.java
@@ -0,0 +1,40 @@
+
+package com.mycompany.mavenproject1;
+
+
+import static java.lang.Math.cbrt;
+public class NewMain {
+
+    
+    
+    public static void main(String[] args) {
+     float sol;
+     float soltemp;
+     
+     
+     
+        for (double i = 1; i < Double.MAX_VALUE; i++) {
+            
+            for (double j = 2; j < i; j++) {
+                
+                sol = (float) cbrt((i*i*i) + (j*j*j));
+                soltemp = sol;
+                
+                while(soltemp>1){
+                soltemp = soltemp-1;                
+                }
+                
+                if (soltemp == 1) {                
+                    System.out.println(i+"  "+ j+"  " + sol);                    
+                    System.out.println(sol+" es un número sin decimales");
+                    System.out.println("He roto las leyes de las matemáticas");
+                }
+            }
+        }
+        
+        
+        
+        
+    }
+    
+}
